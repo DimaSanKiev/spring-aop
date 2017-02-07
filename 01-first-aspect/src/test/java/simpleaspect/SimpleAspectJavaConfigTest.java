2f2b78676b5608bda1b55aa02198240006736e01
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2017-02-07 22:06:59 +0200

    Update pom.xml and classes

--- 01-first-aspect/src/test/java/simpleaspect/SimpleAspectJavaConfigTest.java
+++ 01-first-aspect/src/test/java/simpleaspect/SimpleAspectJavaConfigTest.java
@@ -1,30 +1,30 @@
 package simpleaspect;
 
-import static org.junit.Assert.*;
-
+import configuration.SimpleAspectConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import configuration.SimpleAspectConfiguration;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = SimpleAspectConfiguration.class)
 public class SimpleAspectJavaConfigTest {
 
-	@Autowired
-	TracingAspect tracingAspect;
+    @Autowired
+    private TracingAspect tracingAspect;
 
-	@Autowired
-	SimpleService simpleService;
+    @Autowired
+    private SimpleService simpleService;
 
-	@Test
-	public void aspectIsCalled() {
-		assertFalse(tracingAspect.isEnteringCalled());
-		simpleService.doSomething();
-		assertTrue(tracingAspect.isEnteringCalled());
-	}
+    @Test
+    public void aspectIsCalled() {
+        assertFalse(tracingAspect.isEnteringCalled());
+        simpleService.doSomething();
+        assertTrue(tracingAspect.isEnteringCalled());
+    }
 
 }
--- 01-first-aspect/src/test/java/simpleaspect/SimpleAspectXMLTest.java
+++ 01-first-aspect/src/test/java/simpleaspect/SimpleAspectXMLTest.java
@@ -1,28 +1,29 @@
 package simpleaspect;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/simpleaspect.xml")
 public class SimpleAspectXMLTest {
 
-	@Autowired
-	TracingAspect tracingAspect;
+    @Autowired
+    private TracingAspect tracingAspect;
 
-	@Autowired
-	SimpleService simpleService;
+    @Autowired
+    private SimpleService simpleService;
 
-	@Test
-	public void aspectIsCalled() {
-		assertFalse(tracingAspect.isEnteringCalled());
-		simpleService.doSomething();
-		assertTrue(tracingAspect.isEnteringCalled());
-	}
+    @Test
+    public void aspectIsCalled() {
+        assertFalse(tracingAspect.isEnteringCalled());
+        simpleService.doSomething();
+        assertTrue(tracingAspect.isEnteringCalled());
+    }
 
 }
